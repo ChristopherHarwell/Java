856de1bfef7fcc7a484d127275204b295661c36a
Spacecase94 <45679995+Spacecase94@users.noreply.github.com>
2019-02-25 05:53:03 -0500

    Create JavaCalc.java

---
+++ JavaCalc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class JavaCalc {
+
+	public static void main(String[] args) {
+		Scanner chris = new Scanner(System.in);
+		double fNum,sNum,answer;
+		System.out.println("Enter the first number: ");
+		fNum = chris.nextDouble();
+		System.out.println("Enter the second number: ");
+		sNum = chris.nextDouble();
+		answer = fNum + sNum;
+		System.out.println(answer);
+		
+		
+		
+		
+		
+
+	}
+
+}
